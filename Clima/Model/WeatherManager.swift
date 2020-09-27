a455f24a2b1825e889d717ed79449fd6fc884db6
Shawn Williams <jr.william09@gmail.com>
2020-09-27 18:08:42 -0500

    weather api call is working but will refactor to use closures

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,10 +9,33 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL: String = "http://api.openweathermap.org/data/2.5/weather?appid=8e643839c556d5f4034ed6045a26e695&units=imperial"
+    let weatherURL: String = "https://api.openweathermap.org/data/2.5/weather?appid=8e643839c556d5f4034ed6045a26e695&units=imperial"
     
     func fetchWeather(cityName: String){
         let urlString = "\(self.weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String)  {
+        //1. Create a URL
+        if let url = URL(string: urlString) {
+            //2. Create a URL Session
+            let session = URLSession(configuration: .default)
+            //3. Give the sesssion a task
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            //4. Start the task
+            task.resume()
+        }
+    }
+    func handle(data: Data?, response: URLResponse?, error:  Error?) -> Void {
+        if error != nil {
+            print(error!)
+            return
+        }
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString!)
+        }
+
     }
 }
