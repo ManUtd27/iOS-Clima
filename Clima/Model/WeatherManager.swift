2febbbff42223c83d2657258fe420e8aec10c795
Shawn Williams <jr.william09@gmail.com>
2020-09-27 19:04:23 -0500

    made called funciton in perform request a closure

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -22,20 +22,18 @@ struct WeatherManager {
             //2. Create a URL Session
             let session = URLSession(configuration: .default)
             //3. Give the sesssion a task
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString!)
+                }
+            }
             //4. Start the task
             task.resume()
         }
-    }
-    func handle(data: Data?, response: URLResponse?, error:  Error?) -> Void {
-        if error != nil {
-            print(error!)
-            return
-        }
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString!)
-        }
-
     }
 }
