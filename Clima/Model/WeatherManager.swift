ab08bca8487de213ab570a0ccca7657a52fa6f7c
Shawn Williams <jr.william09@gmail.com>
2020-09-28 05:30:18 -0500

    parsed JOSN data with a JSON decoder while using a Weather Struct to represent my JSON data for the decoder

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,10 +9,13 @@
 import Foundation
 
 struct WeatherManager {
+    // Set Open Weather API URL
     let weatherURL: String = "https://api.openweathermap.org/data/2.5/weather?appid=8e643839c556d5f4034ed6045a26e695&units=imperial"
     
     func fetchWeather(cityName: String){
+        // Get Users input and update create custom Open Weather URL with users location input
         let urlString = "\(self.weatherURL)&q=\(cityName)"
+        // Start the action request
         performRequest(urlString: urlString)
     }
     
@@ -28,12 +31,21 @@ struct WeatherManager {
                     return
                 }
                 if let safeData = data {
-                    let dataString = String(data: safeData, encoding: .utf8)
-                    print(dataString!)
+                    // Parse JSON Data
+                    self.parseJSON(weatherData: safeData)
                 }
             }
             //4. Start the task
             task.resume()
+        }
+    }
+    func parseJSON(weatherData: Data) -> Void {
+        let decoder = JSONDecoder()
+         do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+         } catch {
+            print(error)
         }
     }
 }
